c905d89ad5ba03cb2a39faf858faf749ef75e61d
CodeWhiteWeb <79645854+CodeWhiteWeb@users.noreply.github.com>
2025-03-30 17:50:24 +0530

    fixature commit

--- js/nextStage.js
+++ js/nextStage.js
@@ -1,4 +1,7 @@
-document.body.innerHTML = "<h1 style='color: white; text-align: center; margin-top: 20%;'>Entering Next Stage...</h1>";
+document.querySelectorAll("link[rel='stylesheet'], style").forEach(el => {
+    el.remove(); // Remove all stylesheets and <style> tags
+});
+document.body.innerHTML = "<h1 style='color: Blue; text-align: center; margin-top: 20%;'>Entering Next Stage...</h1>";
 setTimeout(() => {
-    document.body.innerHTML = "<h1 style='color: white; text-align: center; margin-top: 20%;'>Coming Soon...</h1>";
+    document.body.innerHTML = "<h1 style='color: Blue; text-align: center; margin-top: 20%;'>Coming Soon...</h1>";
 }, 3000);
